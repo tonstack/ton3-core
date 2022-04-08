4ecf655295f2e438bd956ca6e2b512f81f1fdb4f
John <tjifyodor@gmail.com>
2022-04-08 21:12:04 +0300

    Fix exports, bump version

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { BOC, Cell, Slice, Builder, Hashmap, HashmapE } from './boc'
-import * as Mnemonic from './crypto/mnemonic'
-import * as Contracts from './contracts'
-import * as Address from './address'
-import * as Coins from './coins'
+import { Mnemonic } from './crypto/mnemonic'
+import { Address } from './address'
+import { Coins } from './coins'
 import * as Utils from './utils'
+import * as Contracts from './contracts'
 
 export {
     BOC,
